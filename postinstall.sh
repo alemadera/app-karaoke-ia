cc24d151f037b69d28af91f803af72578858db2c
Alejandra Madera <139291700+alemadera@users.noreply.github.com>
2025-05-31 15:41:01 -0500

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-
-# Instalar Demucs directamente desde pip
-pip install -U demucs
-
-# Descargar modelo por defecto de Demucs (htdemucs)
-python3 -m demucs --dl
+echo "Postinstall script ejecutado."
